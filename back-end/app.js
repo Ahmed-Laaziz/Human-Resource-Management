10b0379ff07577db587616c5380347fa3c135faf
Laaziz <laazizahmed72@gmail.com>
2023-11-03 14:44:56 +0100

    changes

--- back-end/app.js
+++ back-end/app.js
@@ -22,8 +22,8 @@ var notifRouter = require('./routes/notifications/notification');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // Enable CORS middleware
 app.use(cors(
